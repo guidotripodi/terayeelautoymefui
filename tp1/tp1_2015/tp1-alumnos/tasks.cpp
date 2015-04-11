f654b9da2ba5f4d091125fe42d012482f717b65d
Guido Tripodi <guido.tripodi@hotmail.com>
2015-04-11 14:53:05 -0300

    subiendo tp latexeado!

---
+++ tp1/tp1_2015/tp1-alumnos/tasks.cpp
@@ -0,0 +1,43 @@
+#include "tasks.h"
+#include <iostream>
+using namespace std;
+
+void TaskCPU(int pid, vector<int> params) { // params: n
+	uso_CPU(pid, params[0]); // Uso el CPU n milisegundos.
+}
+
+void TaskIO(int pid, vector<int> params) { // params: ms_pid, ms_io,
+	uso_CPU(pid, params[0]); // Uso el CPU ms_pid milisegundos.
+	uso_IO(pid, params[1]); // Uso IO ms_io milisegundos.
+}
+
+void TaskAlterno(int pid, vector<int> params) { // params: ms_pid, ms_io, ms_pid, ...
+	for(int i = 0; i < (int)params.size(); i++) {
+		if (i % 2 == 0) uso_CPU(pid, params[i]);
+		else uso_IO(pid, params[i]);
+	}
+}
+
+void TaskConBloqueo(int pid, vector<int> params) {
+	int tiempo = params[0];
+	int inicioBloq = params[1];
+	int finBloq = params[2];
+
+	if(inicioBloq > tiempo){
+		uso_CPU(pid, tiempo);
+	}else{
+		uso_CPU(pid,inicioBloq-2);
+		uso_IO(pid,finBloq-inicioBloq+1);
+		uso_CPU(pid,tiempo-finBloq);
+	}
+}
+
+void tasks_init(void) {
+	/* Todos los tipos de tareas se deben registrar acá para poder ser usadas.
+	 * El segundo parámetro indica la cantidad de parámetros que recibe la tarea
+	 * como un vector de enteros, o -1 para una cantidad de parámetros variable. */
+	register_task(TaskCPU, 1);
+	register_task(TaskIO, 2);
+	register_task(TaskAlterno, -1);
+	register_task(TaskConBloqueo,3);
+}
