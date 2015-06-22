901a3940dc26354652c0c6ba8b205697129b0ac3
agustin.rodriguez <agustin.rodriguez@etermax.com>
2015-06-22 17:47:22 -0300

    Arreglo de codigo

--- tp3/srv.c
+++ tp3/srv.c
@@ -15,7 +15,7 @@ void servidor(int mi_cliente)
     int n_ranks, mi_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &mi_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
-    
+
     /*VARIABLES DECLARADAS PARA CONEXION SRV SRV*/
     int tengo_salida = FALSE;
     int mi_secuencia;
@@ -32,14 +32,13 @@ void servidor(int mi_cliente)
     for (i = 0; i < n_ranks; i++){
         faltan_responder[i] = FALSE;
     }
-    
+
     while( ! listo_para_salir ) {
-        
         MPI_Recv(&buffer, 1, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
         origen = status.MPI_SOURCE;
         tag = status.MPI_TAG;
-    
-    /*CONEXION SRV CLI*/    
+
+        /*CONEXION SRV CLI*/
         if (tag == TAG_PEDIDO) {
             assert(origen == mi_cliente);
             assert(tengo_salida == FALSE);
@@ -47,23 +46,21 @@ void servidor(int mi_cliente)
             debug("Mi cliente solicita acceso exclusivo");
             hay_pedido_local = TRUE;
             mi_secuencia = secuencia_maxima + 1;
-            cantidad_servidores = n_ranks -1;
-            if (n_ranks == 1){
+            cantidad_servidores = n_ranks - 1;
+
+            if (n_ranks == 1) {
                 debug("Dándole permiso");
                 tengo_salida = TRUE;
                 MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
-            }
-            else {
-                for (i = 0; i < n_ranks; i++){
+            } else {
+                for (i = 0; i < n_ranks; i++) {
                     if (i != mi_rank){
-                       debug("Pido acceso a todos");
+                        debug("Pido acceso a todos");
                         MPI_Send(&mi_secuencia, 1, MPI_INT, i*2, TAG_REQUEST, COMM_WORLD);
                     }
                 }
             }
-        }
-        
-        else if (tag == TAG_LIBERO) {
+        } else if (tag == TAG_LIBERO) {
             assert(origen == mi_cliente);
             assert(hay_pedido_local == TRUE);
             assert(tengo_salida == TRUE);
@@ -71,15 +68,13 @@ void servidor(int mi_cliente)
             tengo_salida = FALSE;
             debug("Mi cliente libera su acceso exclusivo");
             for (i = 0; i < n_ranks; i++){
-                if ( faltan_responder[i] == TRUE ){
+                if ( faltan_responder[i] == TRUE ) {
                     debug("Respondo pedidos guardados");
                     MPI_Send(NULL, 0, MPI_INT, i*2, TAG_REPLY, COMM_WORLD);
                     faltan_responder[i] = FALSE;
                 }
             }
-        }
-        
-        else if (tag == TAG_TERMINE) {
+        } else if (tag == TAG_TERMINE) {
             assert(origen == mi_cliente);
             assert(hay_pedido_local == FALSE);
             assert(tengo_salida == FALSE);
@@ -92,46 +87,43 @@ void servidor(int mi_cliente)
                 }
             }
         }
+
         /*CONEXION SRV SRV*/
         if (tag == TAG_REQUEST) {
             if(buffer > secuencia_maxima) secuencia_maxima = buffer;
             debug("Me pidieron el recurso");
-            /*AGREGUE ESTE CASO, PORQ SE DABA QUE ESTABA TERMINANDO EL CLIENTE Y SE SACABA EL 
+
+            /*AGREGUE ESTE CASO, PORQ SE DABA QUE ESTABA TERMINANDO EL CLIENTE Y SE SACABA EL
             PEDIDO LOCAL Y ENTRABA NI A LA PARTE VERDADERA NI A LA FALSA*/
-            if(tengo_salida == TRUE){
+            if (tengo_salida == TRUE){
                 debug("Tengo el lock, tiene que esperar, guardo el pedido");
                 assert(faltan_responder[origen/2] == FALSE);
                 faltan_responder[origen/2] = TRUE;
-            }
-            else if(!hay_pedido_local){
+            } else if(!hay_pedido_local) {
                 debug("Lo otorgo por que no lo necesito");
                 MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
-            }
-                else {
-                    /* AGREGUE EL CASO DE SI LA SECUENCIA ES LA MISMA, COSA QUE PASABA AL AGREGAR
-                    DOS SERVIDORES AL MISMO TIEMPO*/
-                    if (buffer < mi_secuencia || (buffer == mi_secuencia && (origen/2) < mi_rank) ){
-                        debug("Lo otorgo por que tiene mayor prioridad");
-                        MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
-                    }
-                    else {
-                        debug("Tiene menor prioridad, no lo otorgo y guardo el pedido");
-                        assert(faltan_responder[origen/2] == FALSE);
-                        faltan_responder[origen/2] = TRUE;
-                    }
+            } else {
+                /* AGREGUE EL CASO DE SI LA SECUENCIA ES LA MISMA, COSA QUE PASABA AL AGREGAR
+                DOS SERVIDORES AL MISMO TIEMPO*/
+                if (buffer < mi_secuencia || (buffer == mi_secuencia && (origen/2) < mi_rank) ) {
+                    debug("Lo otorgo por que tiene mayor prioridad");
+                    MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
+                } else {
+                    debug("Tiene menor prioridad, no lo otorgo y guardo el pedido");
+                    assert(faltan_responder[origen/2] == FALSE);
+                    faltan_responder[origen/2] = TRUE;
                 }
-        }
-        if (tag == TAG_REPLY) {
+            }
+        } else if (tag == TAG_REPLY) {
             assert(tengo_salida == FALSE); //ESTE ES EL QUE AGREGUE
             assert(hay_pedido_local == TRUE);
             cantidad_servidores --;
-            if (cantidad_servidores == 0){
+            if (cantidad_servidores == 0) {
                 tengo_salida = TRUE;
                 debug("Dándole permiso");
                 MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
             }
-        }
-        if (tag == TAG_SIN_CLIENTE) {
+        } else if (tag == TAG_SIN_CLIENTE) {
             clientes--;
         }
 
@@ -145,15 +137,15 @@ void servidor(int mi_cliente)
         MPI_Recv(&buffer, 1, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
         origen = status.MPI_SOURCE;
         tag = status.MPI_TAG;
-        
+
         if (tag == TAG_REQUEST) {
             debug("Otorgo por que ya no tengo cliente");
             MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
-        }        
+        }
 
         if (tag == TAG_SIN_CLIENTE) {
             clientes--;
         }
     }*/
-    
+
 }
