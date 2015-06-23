97d9b10821677cefc5d56261bacee2932ada66f3
guidotripodi <guido.tripodi@hotmail.com>
2015-06-23 19:54:21 -0300

    tp3 terminado

--- tp3/srv.c
+++ tp3/srv.c
@@ -17,16 +17,16 @@ void servidor(int mi_cliente)
     MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
 
     /*VARIABLES DECLARADAS PARA CONEXION SRV SRV*/
-    int tengo_salida = FALSE;
-    int mi_secuencia;
+    int tengo_seccion_critica = FALSE; // Indica cuando el cliente esta en la seccion critica
+    int mi_secuencia; //indica el valor de secuencia que se enviara para tener un order de prioridad
     int buffer;
-    int secuencia_maxima = 0;
-    int cantidad_servidores;
-    int clientes =  n_ranks;
-    int faltan_responder[n_ranks/2];
+    int secuencia_maxima = 0; //sera el valor del ultimo pedido de servidor que se realizo
+    int reply_faltantes; // la cantidad de reply que me faltan para poder otorgar la seccion critica
+    int clientes_activos =  n_ranks/2;
+    int faltan_responder[n_ranks/2]; //son los servidores a los que me faltan dar señal de reply
 
     /*INICIALIZACION DE VARIABLES*/
-    mi_rank = mi_rank/2;
+    mi_rank = mi_rank/2; //Esta se divide por dos ya que los servidores son solo los pares
     n_ranks = n_ranks/2; //Esta se divide por dos ya que los servidores son solo los pares
     int i;
     for (i = 0; i < n_ranks; i++){
@@ -35,27 +35,30 @@ void servidor(int mi_cliente)
 
     while( ! listo_para_salir ) {
         MPI_Recv(&buffer, 1, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
-        origen = status.MPI_SOURCE;
+        origen = status.MPI_SOURCE; //Es el rank que recibi
         tag = status.MPI_TAG;
 
         /*CONEXION SRV CLI*/
         if (tag == TAG_PEDIDO) {
+            //Me llega un pedido de mi cliente para entrar a la seccion critica
             assert(origen == mi_cliente);
-            assert(tengo_salida == FALSE);
+            assert(tengo_seccion_critica == FALSE);
             assert(hay_pedido_local == FALSE);
             debug("Mi cliente solicita acceso exclusivo");
             hay_pedido_local = TRUE;
             mi_secuencia = secuencia_maxima + 1;
-            cantidad_servidores = n_ranks - 1;
+            reply_faltantes = n_ranks - 1;
 
             /*Una vez que el cliente solicita un pedido para uso de seccion critica se chequea si es el
             unico cli-srv activo o no, si es el unico se le otorga permiso, si no lo es se envia una señal de
             req a todos los servidores con el numero de secuencia */
             if (n_ranks == 1) {
+                //solo hay uno
                 debug("Dándole permiso");
-                tengo_salida = TRUE;
+                tengo_seccion_critica = TRUE;
                 MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
             } else {
+                //hay mas de uno. Envio request a los demas con tag request.
                 for (i = 0; i < n_ranks; i++) {
                     if (i != mi_rank){
                         debug("Pido acceso a todos");
@@ -69,9 +72,9 @@ void servidor(int mi_cliente)
             los servidores que solicitaron uso exclusivo */
             assert(origen == mi_cliente);
             assert(hay_pedido_local == TRUE);
-            assert(tengo_salida == TRUE);
+            assert(tengo_seccion_critica == TRUE);
             hay_pedido_local = FALSE;
-            tengo_salida = FALSE;
+            tengo_seccion_critica = FALSE;
             debug("Mi cliente libera su acceso exclusivo");
             for (i = 0; i < n_ranks; i++){
                 if ( faltan_responder[i] == TRUE ) {
@@ -82,12 +85,11 @@ void servidor(int mi_cliente)
             }
         } else if (tag == TAG_TERMINE) {
             /* Cuando finaliza el cliente se envia una señal de SIN_CLIENTE a los demas
-            servidores */
+            servidores para que disminuyan su variable clientes_activos*/
             assert(origen == mi_cliente);
             assert(hay_pedido_local == FALSE);
-            assert(tengo_salida == FALSE);
-            assert(listo_para_salir == FALSE);
-            clientes--;
+            assert(tengo_seccion_critica == FALSE);
+            clientes_activos--;
             debug("Mi cliente avisa que terminó");
             for (i = 0; i < n_ranks; i++){
                 if (i != mi_rank) {
@@ -107,18 +109,21 @@ void servidor(int mi_cliente)
             debug("Me pidieron el recurso");
 
 
-            if (tengo_salida == TRUE){
+            if (tengo_seccion_critica == TRUE){
+                //tengo la seccion critica, nadie me la sacara!
                 debug("Tengo el lock, tiene que esperar, guardo el pedido");
                 assert(faltan_responder[origen/2] == FALSE);
-                faltan_responder[origen/2] = TRUE;
+                faltan_responder[origen/2] = TRUE; // le respondo despues
             } else if(!hay_pedido_local) {
                 debug("Lo otorgo por que no lo necesito");
                 MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
             } else {
                 if (buffer < mi_secuencia || (buffer == mi_secuencia && (origen/2) < mi_rank) ) {
+                    //Si mi secuencia es mas grande o , si son iguales y tengo rank mayor, le mando reply 
                     debug("Lo otorgo por que tiene mayor prioridad");
                     MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
                 } else {
+                    //tengo mayor prioridad, que espere!
                     debug("Tiene menor prioridad, no lo otorgo y guardo el pedido");
                     assert(faltan_responder[origen/2] == FALSE);
                     faltan_responder[origen/2] = TRUE;
@@ -128,37 +133,25 @@ void servidor(int mi_cliente)
 
             /*Si se recibe un reply, se chequea la cantidad que ya se obtuvieron, en caso de
             tener todos los reply de los srv se otorga el permiso al cliente*/
-            assert(tengo_salida == FALSE);
+            assert(tengo_seccion_critica == FALSE);
             assert(hay_pedido_local == TRUE);
-            cantidad_servidores --;
-            if (cantidad_servidores == 0) {
-                tengo_salida = TRUE;
+            reply_faltantes --; //uno menos...
+            if (reply_faltantes == 0) {
+                //al fin me respondieron todos! puedo otorgar seccion critica
+                tengo_seccion_critica = TRUE;
                 debug("Dándole permiso");
+                //envio a mi cliente el permiso para acceder
                 MPI_Send(NULL, 0, MPI_INT, mi_cliente, TAG_OTORGADO, COMM_WORLD);
             }
         } else if (tag == TAG_SIN_CLIENTE) {
-            clientes--;
+            //se fue algun cliente de algun server, disminuyo la cantidad de clientes activos
+            clientes_activos--;
         }
 
-        if (clientes == n_ranks) {
+        if (clientes_activos == 0) {
+            //A ningun server le queda cliente, cierro el servidor.  
+            assert(listo_para_salir == FALSE);
             listo_para_salir = TRUE;
         }
     }
-    /*ESTO LO HICE YA QUE AL NO TENER CLIENTE EL SERVER QUEDA AL PEDO Y NO OTORGA NADA*/
-/*
-    while(clientes > 0) {
-        MPI_Recv(&buffer, 1, MPI_INT, ANY_SOURCE, ANY_TAG, COMM_WORLD, &status);
-        origen = status.MPI_SOURCE;
-        tag = status.MPI_TAG;
-
-        if (tag == TAG_REQUEST) {
-            debug("Otorgo por que ya no tengo cliente");
-            MPI_Send(NULL, 0, MPI_INT, origen, TAG_REPLY, COMM_WORLD);
-        }
-
-        if (tag == TAG_SIN_CLIENTE) {
-            clientes--;
-        }
-    }*/
-
 }
